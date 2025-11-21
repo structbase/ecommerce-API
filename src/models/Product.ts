5aaa48edd9092098d9dbef2cec0e8fa60c8d4a11
Abenezer <abitysank@gmail.com>
2025-11-21 12:58:52 -0500

    fix to configuration and Products implementation done... maybe

---
+++ src/models/Product.ts
@@ -0,0 +1,32 @@
+export class Product {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+
+    constructor(
+        id: number,
+        title: string,
+        description: string,
+        category: string,
+        price: number,
+        discountPercentage: number
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.discountPercentage = discountPercentage;
+    }
+    displayDetails(): string {
+        return `Product name: `;
+    }
+    getPriceWithDiscount(): string {
+        const discounted = this.price * (1 - this.discountPercentage / 100);
+        return `Discounted Price: ${discounted.toFixed(2)}`;
+    }
+}
+
