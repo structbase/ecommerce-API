56faa5eb23fae0e38440d30a6a3e797889ca5a3a
Abenezer <abitysank@gmail.com>
2025-11-23 23:01:15 -0500

    fix issue of returning any by adding types to the APIby creating interface

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -3,16 +3,29 @@ import { handleError, CustomError } from "./errorHandler";
 //     .then((res) => res.json())
 //     .then(console.log);
 
-export async function fetchData() {
+export interface ProductData {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+}
+
+export interface ApiResponse {
+    products: ProductData[];
+}
+
+export async function fetchData(): Promise<ApiResponse> {
     try {
         const response = await fetch("https://dummyjson.com/products");
         if (!response.ok) {
             throw new CustomError("Failed to fetch products", response.status);
         }
-        const data = await response.json();
-        return data; 
+        const data = (await response.json()) as ApiResponse;
+        return data;
     } catch (error) {
         handleError(error);
-        throw error; 
+        throw error;
     }
 }
